c1300ae746bf5e058dee6d6221621d97da073af4
Aleksey <akhasano@gmail.com>
2025-12-17 21:26:56 +0300

    Setup for neovide

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,17 @@ vim.lsp.config.ansiblels = {
   filetypes = { 'yaml', 'yml' }
 }
 
+if vim.g.neovide then
+    -- Put anything you want to happen only in Neovide here
+  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h11"
+  -- Вставка в нормальном и визуальном режимах
+  vim.keymap.set({'n', 'v'}, '<C-S-V>', '"+p')
+  -- Вставка в режиме вставки (Insert mode)
+  vim.keymap.set('i', '<C-S-V>', '<C-R>+')
+  -- Вставка в командной строке
+  vim.keymap.set('c', '<C-S-V>', '<C-R>+')
+end
+
 -- Переопределяем группы для diff
 local highlights = {
   DiffChange = { bg = "#4c4c4c", fg = "NONE", bold = true },
