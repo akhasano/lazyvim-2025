915538d89dcbf40b01784dec6b41455419133283
Aleksey <akhasano@gmail.com>
2026-04-02 22:56:31 +0300

    fixed clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,30 +6,30 @@
 -- vim.cmd('set norelativenumber')
 
 vim.lsp.config.ansiblels = {
-  filetypes = { 'yaml', 'yml' }
+	filetypes = { "yaml", "yml" },
 }
 
 if vim.g.neovide then
-    -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "JetBrainsMono Nerd Font Mono:h11"
-  -- Вставка в нормальном и визуальном режимах
-  vim.keymap.set({'n', 'v'}, '<C-S-V>', '"+p')
-  -- Вставка в режиме вставки (Insert mode)
-  vim.keymap.set('i', '<C-S-V>', '<C-R>+')
-  -- Вставка в командной строке
-  vim.keymap.set('c', '<C-S-V>', '<C-R>+')
+	-- Put anything you want to happen only in Neovide here
+	vim.o.guifont = "JetBrainsMono Nerd Font Mono:h11"
+	-- Вставка в нормальном и визуальном режимах
+	vim.keymap.set({ "n", "v" }, "<C-S-V>", '"+p')
+	-- Вставка в режиме вставки (Insert mode)
+	vim.keymap.set("i", "<C-S-V>", "<C-R>+")
+	-- Вставка в командной строке
+	vim.keymap.set("c", "<C-S-V>", "<C-R>+")
 end
 
 -- Переопределяем группы для diff
 local highlights = {
-  DiffChange = { bg = "#4c4c4c", fg = "NONE", bold = true },
-  DiffText   = { bg = "#FFA500", fg = "#000000", bold = true },
-  DiffAdd    = { bg = "#4C784C", fg = "NONE" },
-  DiffDelete = { bg = "#804040", fg = "NONE" },
+	DiffChange = { bg = "#4c4c4c", fg = "NONE", bold = true },
+	DiffText = { bg = "#FFA500", fg = "#000000", bold = true },
+	DiffAdd = { bg = "#4C784C", fg = "NONE" },
+	DiffDelete = { bg = "#804040", fg = "NONE" },
 }
 
 for group, opts in pairs(highlights) do
-  vim.api.nvim_set_hl(0, group, opts)
+	vim.api.nvim_set_hl(0, group, opts)
 end
 
 vim.opt.winbar = "%=%m %f"
@@ -37,3 +37,4 @@ vim.opt.winbar = "%=%m %f"
 -- Recommended Neovim options - avante.nvim
 -- views can only be fully collapsed with the global statusline
 vim.opt.laststatus = 3
+vim.opt.clipboard = "unnamedplus"
